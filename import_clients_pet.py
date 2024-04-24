4aaabf2bbb74868faf692339cc9a442f0db491e7
AlinaLosk <161211124+AlinaLosk@users.noreply.github.com>
2024-04-24 18:23:35 +0300

    Update import_clients_pet.py

--- import_clients_pet.py
+++ import_clients_pet.py
@@ -2,3 +2,15 @@ from client_pet_friends import Clients
 
 client_1 = Clients ("Иван","Петров","Москва",50)
 print(client_1)
+
+# список будет возвращать информацию об имени, фамилии и городе клиента.
+
+client_2 = Clients ("Зиновий","Эдмундов","Нижний Новгород",50)
+client_3 = Clients ("Аристарх","Ридов","Тюмень",50)
+client_4 = Clients ("Клавдия","Задонская","Липецк",50)
+client_5 = Clients ("Тираида","Иванова","Москва",50)
+
+client_list = [client_2,client_3,client_4,client_5]
+
+for client in client_list:
+    print(client.get_client())
