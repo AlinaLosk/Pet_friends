418af124dfc15ea7ee34a0b69eb48e3a24a44261
AlinaLosk <161211124+AlinaLosk@users.noreply.github.com>
2024-04-24 18:21:58 +0300

    Update client_pet_friends.py

--- client_pet_friends.py
+++ client_pet_friends.py
@@ -7,3 +7,8 @@ class Clients:
 
     def __str__(self):
         return f'Clients : {self.name}, {self.surname}, {self.city}, Баланс : {self.balanсe} руб.'
+        
+ # метод, который будет возвращать информацию только об имени, фамилии и городе клиента.
+    
+    def get_client(self):
+        return f'{self.name} {self.surname},г. {self.city}'
